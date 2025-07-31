e23e66ccbc6c2e4a82f457747c41c7ddf2ff2dfb
Johnny Herrera <jmjaramillo95@gmail.com>
2025-07-30 23:19:17 -0500

    colecciones

--- tipos-de-colecciones/Program.cs
+++ tipos-de-colecciones/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,47 @@ using System.Threading.Tasks;
 
 namespace tipos_de_colecciones
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            /*
+            List<int> numbers = new List<int>();
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(1);
+            numbers.Remove(1);
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            /*
+            Hashtable numbers = new Hashtable();
+            numbers.Add(1, "1a");
+            numbers.Add(2, "2");
+            numbers.Add(3, "3");
+            numbers.Add(4, "4");
+
+            foreach (var i in numbers.Values)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            Dictionary<int, string> numbers = new Dictionary<int, string>();
+            numbers.Add(1, "1a");
+            numbers.Add(2, "2");
+            numbers.Add(3, "3");
+            numbers.Add(4, "4");
+
+            foreach (var i in numbers.Values)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
